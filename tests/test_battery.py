9ae9a796fd5d7a95c3a2afbe0b987ebfc69f3679
H3xept <h3xept@gmail.com>
2021-11-19 15:27:44 +0100

    Added proper battery example

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -2,6 +2,6 @@ from PowerModels.PowerTrain.Battery import Battery
 
 def test_battery():
     controls = [0.2, 0.2, 0.2, 0.2]
-    batt = Battery(25.0, 0, 0.004 / 3600)
-    voltage, dod = batt.new_control(controls) # List of [0-1] values [0.00001, ..., 0.999999]
+    batt = Battery(25.0, 0.3)
+    voltage, dod = batt.new_control(controls, 1 / 3600) # List of [0-1] values [0.00001, ..., 0.999999]
     print(voltage, dod)
